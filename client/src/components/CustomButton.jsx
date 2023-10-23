e30f972e12b4657aa6879f828d3fb5328409a5a6
Brayahn <brayahnmainah@gmail.com>
2023-10-23 04:21:58 +0300

    button optimization

--- client/src/components/CustomButton.jsx
+++ client/src/components/CustomButton.jsx
@@ -2,7 +2,10 @@ import React from 'react'
 
 const CustomButton = () => {
   return (
-    <div>CustomButton</div>
+   <button
+   >
+
+   </button>
   )
 }
 
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -27,7 +27,13 @@ const Home = () => {
                     <p className='max-w-md font-normal text-gray-600 text-base'>
                         Create your unique and exclusive shirt with our brand new 3D customization tool. <strong>Unleash your Wildest Imagination</strong>{" "} and define your own style.
                     </p>
-                    <CustomButton/>
+                    <CustomButton
+                    type='filled'
+                    title='Customize It'
+                    handleClick={() =>state.intro=false}
+                    customStyles="w-fit px-4 py-2.5 font-bold text-sm"
+
+                    />
                 </motion.div>
             </motion.section>
         )}
