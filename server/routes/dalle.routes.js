23d7a5d8c15dc23a8be1e229dcb43c1e15d51fb1
Brayahn <brayahnmainah@gmail.com>
2023-11-08 05:36:45 +0300

    backend server setup complete

--- server/routes/dalle.routes.js
+++ server/routes/dalle.routes.js
@@ -7,11 +7,31 @@ dotenv.config();
 const router = express.Router();
 
 const openai = new OpenAI({
-  apiKey: process.env.OPENAI_API_KEY // This is also the default, can be omitted
+  apiKey: process.env.OPENAI_API_KEY, // This is also the default, can be omitted
 });
 
 router.route("/").get((req, res) => {
   res.status(200).json({ message: "Hello from DALL.E Routes" });
+});
+
+router.route("/").post(async (req, res) => {
+  try {
+    const { prompt } = req.body;
+
+    const response = await openai.createImage({
+      prompt,
+      n: 1,
+      size: "1024x1024",
+      response_format: "b64_json",
+    });
+
+    const image = response.data.data[0].b64_json;
+
+    res.status(200).json({ photo: image });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Something went wrong" });
+  }
 });
 
 export default router;
