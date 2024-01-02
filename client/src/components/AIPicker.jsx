a3d2ac086cf98a6ff691e9f6ef7a9436f69c04c4
Brayahn <brayahnmainah@gmail.com>
2024-01-02 06:01:49 +0300

    AI Picker complete

--- client/src/components/AIPicker.jsx
+++ client/src/components/AIPicker.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import CustomButton from './CustomButton'
+import CustomButton from './CustomButton';
 
 const AIPicker = ({prompt, setPrompt, generateImg, handleSubmit}) => {
   return (
@@ -10,9 +10,34 @@ const AIPicker = ({prompt, setPrompt, generateImg, handleSubmit}) => {
       value={prompt}
       onChange={(e)=> setPrompt(e.target.value)}
       className='aipicker-textarea'
-      >
+      />
 
-      </textarea>
+      <div className='flex flex-wrap gap-3'>
+{generateImg? (
+  <CustomButton 
+  type="outline"
+  title="Asking AI..."
+  customStyles="text-xs"
+  />
+) :(
+  <>
+  <CustomButton
+  type="outline"
+  title="AI Logo"
+  handleClick={()=> handleSubmit('logo')}
+  customStyles="text-xs"
+  />
+    <CustomButton
+  type="filled"
+  title="AI Full"
+  handleClick={()=> handleSubmit('full')}
+  customStyles="text-xs"
+  />
+  </>
+)
+}
+      </div>
+
     </div>
   )
 }
